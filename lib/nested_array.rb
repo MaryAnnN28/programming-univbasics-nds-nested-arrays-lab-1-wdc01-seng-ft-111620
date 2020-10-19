4e28616f3f915df317875cff8e763fe3b76c9b92
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 19:45:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -46,7 +46,7 @@ def matrix_lookup(matrix, row, column)
   CONVENTIONAL_PRODUCE,
   ORGANIC_PRODUCE,
 ]
-produce_storage_room[0][1]
+produce_storage_room[1][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
