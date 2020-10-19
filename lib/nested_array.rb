236845ecf344801814813d9fd69b5d859ef81da7
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 19:42:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -39,9 +39,14 @@ def sorted_matrix
   produce_storage_room
 end
 
+# Given any matrix (array of arrays), a row index and a column index,
+# Return the matrix's content at that row and and column
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index,
-  # Return the matrix's content at that row and and column
+  produce_storage_room = [
+  CONVENTIONAL_PRODUCE,
+  ORGANIC_PRODUCE,
+]
+produce_storage_room[0][1]
 end
 
 def matrix_update(matrix, row, column, new_value)
