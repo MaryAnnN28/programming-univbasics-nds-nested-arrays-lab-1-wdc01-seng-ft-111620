5095fd5309fce2e09bac1b6504851d1e57cd57a8
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 19:59:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -50,8 +50,13 @@ matrix_lookup(produce_storage_room, 1, 1)
 matrix_lookup(produce_storage_room, 1, 3)
 end
 
+# Given any matrix (array of arrays), a row index and a column index,
+# Update the matrix location at that row and column to have the value of new_value
+# Return the updated matrix
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index,
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+  produce_storage_room = [
+    CONVENTIONAL_PRODUCE, 
+    ORGANIC_PRODUCE,
+  ]
+  CONVENTIONAL_PRODUCE[2] = "Lemons"
 end
