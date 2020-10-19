48ed436fbdd6db6d09ff3cb97365b3e5fae3aabc
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 20:15:34 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -55,9 +55,9 @@ end
 # Return the updated matrix
 def matrix_update(matrix, row, column, new_value)
   updated_produce_storage = [
-    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
+    ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], 
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
-  produce_storage_room[0][2] = "Lemons"
-  produce_storage_room
+  updated_produce_storage[0][2] = "Lemons"
+  updated_produce_storage
 end
