18524f3e302e6d2286428855ee9ae19715de9655
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-19 20:29:56 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -53,11 +53,15 @@ end
 # Given any matrix (array of arrays), a row index and a column index,
 # Update the matrix location at that row and column to have the value of new_value
 # Return the updated matrix
-produce_storage_room[0][2] = "Lemons"
-produce_storage_room
 
 def matrix_update(matrix, row, column, new_value)
-
-  matrix_update(produce_storage_room, 0, 2, "Onions")
-  matrix_update
+  produce_storage_room = [
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE,
+  ]
+  produce_storage_room[0][2] = "Lemons"
+  produce_storage_room
+end
+matrix_update(produce_storage_room, 0, 2, "Onions")
+matrix_update
 end
